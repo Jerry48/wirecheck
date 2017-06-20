795ca7532c8665baa98d408b979ca97ea687ac24
Jerry <root@iZ2zecrq4k5plfagvhne1xZ.(none)>
2017-06-20 20:59:07 +0800

    upload others

---
+++ db.js
@@ -0,0 +1,22 @@
+// mySql connection pool
+// mySql connection pool
+// copyright@CCFLab.sjtu.edu.cn reserved, 2016
+/*
+ * history:
+ * 2016.03.26, created by Andy.zhou
+ *  
+ */
+
+var database =  (process.env.DB_ENV && 'wirecheck') || 'wirecheck';
+
+var mysql = require('mysql');
+var pool  = mysql.createPool({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'kooling123',
+	database       : database,
+	connectionLimit: 20,
+	queueLimit     : 30
+});
+
+module.exports = pool;
